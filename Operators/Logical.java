4bf3075dd201740829bd5ce6218576c8760caac4
Nidhisaa <senthilnidhi57@gmail.com>
2026-05-25 14:09:57 +0530

    reversed the logic

--- Operators/Logical.java
+++ Operators/Logical.java
@@ -1,10 +1,11 @@
 public class Logical {
     public static void main(String[] args) {
 
-        boolean R = true;
-        int id = 10;
+        boolean x = true;
+        boolean y = false;
 
-        System.out.println("R = " + R);
-        System.out.println("id = " + id);
+        System.out.println("x && y = " + (x && y));
+        System.out.println("x || y = " + (x || y));
+        System.out.println("!x = " + (!x));
     }
 }
