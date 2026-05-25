657b87081c4f5e84b244db53f2522cc423701823
Nidhisaa <senthilnidhi57@gmail.com>
2026-05-25 11:51:59 +0530

    Add DecimalTypes class to demonstrate usage of float and double data types

---
+++ DataTypes/DecimalTypes.java
@@ -0,0 +1,11 @@
+public class DecimalTypes {
+
+    public static void main(String[] args) {
+
+        float cgpa = 8.95f;
+        double pi = 3.1415926535;
+
+        System.out.println(cgpa);
+        System.out.println(pi);
+    }
+}
