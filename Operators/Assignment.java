b4e6c14d5dc2d7d97a35695df4ee9af8a91fcd29
Nidhisaa <senthilnidhi57@gmail.com>
2026-05-25 11:04:57 +0530

    Add Arithmatic and Assignment classes with basic arithmetic operations

---
+++ Operators/Assignment.java
@@ -0,0 +1,23 @@
+public class Assignment {
+    public static void main(String[] args) {
+
+        int a = 10;
+
+        System.out.println("Initial Value = " + a);
+
+        a += 5;
+        System.out.println("a += 5 : " + a);
+
+        a -= 3;
+        System.out.println("a -= 3 : " + a);
+
+        a *= 2;
+        System.out.println("a *= 2 : " + a);
+
+        a /= 4;
+        System.out.println("a /= 4 : " + a);
+
+        a %= 3;
+        System.out.println("a %= 3 : " + a);
+    }
+}
