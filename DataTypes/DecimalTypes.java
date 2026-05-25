3b6ae531e2e0ae10815ac05fedc9891d4075a00f
Nidhisaa <senthilnidhi57@gmail.com>
2026-05-25 11:55:15 +0530

    Update DecimalTypes class to refine float and double values for accuracy

--- DataTypes/DecimalTypes.java
+++ DataTypes/DecimalTypes.java
@@ -2,8 +2,8 @@ public class DecimalTypes {
 
     public static void main(String[] args) {
 
-        float cgpa = 8.95f;
-        double pi = 3.1415926535;
+        float cgpa = 8.933452523455f;
+        double pi = 3.141592659378537835;
 
         System.out.println(cgpa);
         System.out.println(pi);
