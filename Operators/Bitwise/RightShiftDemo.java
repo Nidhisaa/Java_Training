c32857afdbc05d23d73f69a8d1ddcdd81d90b402
Nidhisaa <senthilnidhi57@gmail.com>
2026-05-25 14:42:41 +0530

    Remove Bitwise and DecimalToBinary classes; add RightShiftDemo class to demonstrate bitwise right shift operations

---
+++ Operators/Bitwise/RightShiftDemo.java
@@ -0,0 +1,11 @@
+public class RightShiftDemo {
+    public static void main(String[] args) {
+
+        int a = 16;
+
+        System.out.println("a = " + a);
+        System.out.println("a >> 1 = " + (a >> 1));
+        System.out.println("a >> 2 = " + (a >> 2));
+        System.out.println("a >> 3 = " + (a >> 3));
+    }
+}
