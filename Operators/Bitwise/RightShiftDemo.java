a2b7c6638799eed97d3058f4838155c8c06e6896
Nidhisaa <senthilnidhi57@gmail.com>
2026-05-25 15:25:30 +0530

    updated right shift

--- Operators/Bitwise/RightShiftDemo.java
+++ Operators/Bitwise/RightShiftDemo.java
@@ -1,12 +1,13 @@
-public class RightShiftDemo {
+public class RightShiftDemo{
     public static void main(String[] args) {
-
-        int a = 16;
+        int a = 17;
+        int b = 23;
 
         System.out.println("a = " + a);
-        System.out.println("a >> 1 = " + (a >> 1));
         System.out.println("a >> 2 = " + (a >> 2));
         System.out.println("a >> 3 = " + (a >> 3));
         System.out.println("a << 4 = " + (a << 4));
     }
 }
+
+       
