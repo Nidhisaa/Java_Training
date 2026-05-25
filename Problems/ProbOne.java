b14d12dcdc11aeb3a679e5351f374bff115f1d84
Nidhisaa <senthilnidhi57@gmail.com>
2026-05-25 21:13:55 +0530

    Add ProbOne class to calculate profit based on user input

---
+++ Problems/ProbOne.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class ProbOne{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int profit = (a * b) - (a * c) - 100;
+        System.out.println(profit);
+    }
+}
