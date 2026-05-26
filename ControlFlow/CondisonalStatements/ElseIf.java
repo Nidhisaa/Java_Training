7262b27c98d3610391cfcb8e77aa8ed70b1acc36
Nidhisaa <senthilnidhi57@gmail.com>
2026-05-26 09:51:10 +0530

    Add ElseIf class to evaluate grades based on user input

---
+++ ControlFlow/CondisonalStatements/ElseIf.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class ElseIf {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int score;
+
+        System.out.print("Enter score (0-100): ");
+        if (sc.hasNextInt()) {
+            score = sc.nextInt();
+        } else {
+            System.out.println("Invalid input");
+            sc.close();
+            return;
+        }
+
+        if (score >= 90) {
+            System.out.println("Grade: A"); // Checked 1st
+        } else if (score >= 80) {
+            System.out.println("Grade: B"); // Checked 2nd (only if 1st was false)
+        } else if (score >= 70) {
+            System.out.println("Grade: C"); // Checked 3rd (only if 1st and 2nd were false)
+        } else {
+            System.out.println("Grade: F"); // Runs only if ALL above are false
+        }
+
+        sc.close();
+    }
+}
---
+++ Problems/ProbTwo.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class ProbTwo{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int GT = sc.nextInt();
+        int ben = sc.nextInt();
+        int BB = sc.nextInt();
+
+        int benT = (GT*ben) / 100;
+        System.out.println(benT);
+        
+        GT = GT - benT;
+        int BBT = (GT*BB) / 100;
+        System.out.println(BBT);
+
+        GT = GT - BBT;
+        System.out.println(GT);
+
+        sc.close();
+
+    }
+}
