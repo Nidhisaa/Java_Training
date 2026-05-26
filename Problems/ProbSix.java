5cac0d9fe888aa8cead514e953348d1b1c8d11a9
Nidhisaa <senthilnidhi57@gmail.com>
2026-05-26 12:40:41 +0530

    Add triangle validation logic to ProbSix class

--- Problems/ProbSix.java
+++ Problems/ProbSix.java
@@ -6,13 +6,17 @@ public class ProbSix{
         int b = sc.nextInt();       
         int c = sc.nextInt();
 
-        if ( a == b && a ==c){
+        if ((a+b) > c && (b+c) > a && (a+c) > b){
+            
+            if ( a == b && a ==c){
             System.out.println("Equilateral Triangle");
         }else if (a == b || a == c || b == c){
             System.out.println("Isosceles Triangle");
         }else{
             System.out.println("Scalene Triangle"); 
         }
-       
+    }     else{
+            System.out.println("Not a Triangle");
+        }
     }
 }
