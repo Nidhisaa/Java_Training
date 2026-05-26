8c3fac0781a5f1b7ccff24eafe84bf53679fafd7
Nidhisaa <senthilnidhi57@gmail.com>
2026-05-26 10:15:46 +0530

    Add ProbThree class to check if a number is a palindrome

---
+++ Problems/ProbThree.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class ProbThree {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number (0-9999): ");
+        int num;
+        if (sc.hasNextInt()) {
+            num = sc.nextInt();
+        } else {
+            System.out.println("Invalid number input");
+            sc.close();
+            return;
+        }
+
+            String s = Integer.toString(Math.abs(num));
+            boolean palindrome = s.equals(new StringBuilder(s).reverse().toString());
+
+        if (palindrome) {
+            System.out.println("Safe Unlocked!");
+        } else {
+            System.out.println("Access Denied");
+        }
+
+        sc.close();
+    }
+
+    }
+
+
