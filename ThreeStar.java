00fb974bb335d22fd9cf9318154717e8f6508689
Nidhisaa <senthilnidhi57@gmail.com>
2026-05-27 14:09:05 +0530

    Add @SuppressWarnings annotation for resource management in HallowStar and ThreeStar classes

--- ThreeStar.java
+++ ThreeStar.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 
 
 public class ThreeStar {
+    @SuppressWarnings("ConvertToTryWithResources")
     public static void main(String[] args) {
         
         Scanner sc = new Scanner(System.in);
