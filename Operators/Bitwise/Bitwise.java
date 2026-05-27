da70f35ae2a054d9001f4cd29e1a2f0a49dd7225
Nidhisaa <senthilnidhi57@gmail.com>
2026-05-27 06:16:07 +0530

    Add output for variable values in Bitwise class

--- Operators/Bitwise/Bitwise.java
+++ Operators/Bitwise/Bitwise.java
@@ -4,6 +4,9 @@ public class Bitwise {
         int a = 5;
         int b = 3;
 
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
         System.out.println("a & b = " + (a & b));
         System.out.println("a | b = " + (a | b));
         System.out.println("a ^ b = " + (a ^ b));
