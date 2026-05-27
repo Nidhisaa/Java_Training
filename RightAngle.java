aa92d4f93337f4f90c38768b6c9e9f17e776bc53
Nidhisaa <senthilnidhi57@gmail.com>
2026-05-27 11:49:11 +0530

    basic update of package

---
+++ RightAngle.java
@@ -0,0 +1,18 @@
+
+
+import java.util.Scanner;
+public class RightAngle {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        
+        int size = sc.nextInt();
+        for (int i = 1; i <= size; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
