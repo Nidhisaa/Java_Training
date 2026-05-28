17cc7987c26da7a180cb9a50bd012a242cde70ba
Nidhisaa <senthilnidhi57@gmail.com>
2026-05-28 11:03:33 +0530

    Refactor DiffBtwn class to use try-with-resources for Scanner and add Main class for Devil's Diamond pattern

--- DiffBtwn.java
+++ DiffBtwn.java
@@ -2,14 +2,14 @@ import java.util.Scanner;
 
 public class DiffBtwn {
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.hasNextInt() ? sc.nextInt() : 0;
-		long diff = 0;
-		for (int i = 0; i < n && sc.hasNextLong(); i++) {
-			long x = sc.nextLong();
-			diff += (x % 2 == 0) ? x : -x;
-		}
-		System.out.println(Math.abs(diff));
-		sc.close();
+            try (Scanner sc = new Scanner(System.in)) {
+                int n = sc.hasNextInt() ? sc.nextInt() : 0;
+                long diff = 0;
+                for (int i = 0; i < n && sc.hasNextLong(); i++) {
+                    long x = sc.nextLong();
+                    diff += (x % 2 == 0) ? x : -x;
+                }
+                System.out.println(Math.abs(diff));
+            }
 	}
 }
