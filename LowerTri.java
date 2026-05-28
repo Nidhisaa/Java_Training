46297f2051878681285cc8ef9ed371c235276bab
Nidhisaa <senthilnidhi57@gmail.com>
2026-05-28 14:32:23 +0530

    Add LowerTri class to calculate and print the maximum row sum of a user-defined matrix

---
+++ LowerTri.java
@@ -0,0 +1,32 @@
+import java.util.*;
+public class LowerTri {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[][] = new int[n][n];
+
+
+        boolean check = true;
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                arr[i][j] = sc.nextInt();
+            }
+            
+        }  
+        int max = 0;
+            for(int i=0;i<n;i++){
+                int sum = 0;
+                for(int j=0;j<n;j++){
+                  sum = sum + arr[i][j];
+                }
+               
+                if(sum > max){
+                    max = sum;
+                }
+            }
+           
+                System.out.print(max);
+                sc.close();
+    }
+}
