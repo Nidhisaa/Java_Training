45ba79ffa94af51aa1b1e38becd7197735a77368
Nidhisaa <senthilnidhi57@gmail.com>
2026-05-29 11:17:12 +0530

    Refactor StrRev class to implement string reversal by words and clean up commented code

--- StrRev.java
+++ StrRev.java
@@ -1,5 +1,6 @@
+import java.util.*;
 public class StrRev {
-    public static void main (String[] args) {
+   /* public static void main (String[] args) {
         String str = "Hello, World!";
         String reversedStr = reverseString(str);
         System.out.println("Original String: " + str);
@@ -8,5 +9,23 @@ public class StrRev {
 
     public static String reverseString(String str) {
         return new StringBuilder(str).reverse().toString();
-    }
+    }*/
+
+        public static void main(String[] args){
+            Scanner sc = new Scanner(System.in);
+            String s = sc.nextLine();
+            String rev = "";
+            /*for(int i = s.length() - 1; i >= 0; i--){
+                rev = rev + s.charAt(i);
+            }
+            System.out.println(rev);*/
+
+            
+            // "hellow world " to "world hellow"
+            String[] arr = s.split(" ");
+            for(int i = arr.length - 1; i >= 0; i--){
+                rev = rev + arr[i] + " ";
+            }
+            System.out.println(rev);
+        }
 }
