861ff672379dec11be761a7a888467a1898417e7
Nidhisaa <senthilnidhi57@gmail.com>
2026-05-29 12:24:45 +0530

    Clean up comments and improve readability in FirstNonRep_pr class

--- Problems/FirstNonRep_pr.java
+++ Problems/FirstNonRep_pr.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class FirstNonRep_pr {
 	public static void main(String[] args) {
             try (Scanner sc = new Scanner(System.in)) {
-// input is "teeterson" output is 'r' , simple code to find the first non repeating character in a string
+// input is "teeterson" output is 'r'
                 String str = sc.next();
                 for (int i = 0; i < str.length(); i++) {
                     char c = str.charAt(i);
@@ -17,3 +17,34 @@ public class FirstNonRep_pr {
             }
         }
     }
+
+// input is "teeterson" output is 'r'
+// ANOTHER LOGIC TO SOLVE
+//int [] count = new int[26];
+// for (int i = 0; i < str.length(); i++) {
+// count[str.charAt(i) - 'a']++;
+//}
+// for (int i=0; i<str.lemngth(;i++){) {
+//     System.out.println("Character : "+(char)(i+97)+" Count : "+
+
+//     count[i]) );
+//     }
+
+
+// METH 2
+// for (int i = 0; i < str.length(); i++) {
+//     char c = str.charAt(i);  
+//     if (str.indexOf(c) == str.lastIndexOf(c)) {
+//         System.out.println(c);
+//         break;
+//     }
+// }
+
+
+//METH 3
+// for (int i = 0; i < str.length(); i++) {
+//     if(count[str.charAt(i) - 'a'] == 1) {
+//         System.out.println(str.charAt(i));
+//         break;
+//     }
+// }
