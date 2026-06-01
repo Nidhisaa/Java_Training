22674e272241e961c59f2d1ce1d31ff24d15698d
Nidhisaa <senthilnidhi57@gmail.com>
2026-06-01 16:05:30 +0530

    Add try-catch-finally example to demonstrate exception handling in Java

---
+++ ExceptionHandling/trycatchfinally.java
@@ -0,0 +1,38 @@
+package ExceptionHandling;
+
+import java.util.*;
+public class trycatchfinally {
+    public static void main(String[] args) {
+        try{
+            Scanner sc = new Scanner(System.in);
+            int num = sc.nextInt();  //12.5
+            System.out.println(num);
+        }
+        // common exception that handles all the exception
+        catch(Exception e){
+            System.out.println("Exception handled");
+            System.out.println(e);
+        }
+        // Multiple catch block can be used
+
+        // catch(ArithmeticException e){
+        //     System.out.println("Arithmetic Exception Handled");
+        //     System.out.println(e);
+        // }
+
+        // catch(IndexOutOfBoundsException e){
+        //     System.out.println("Index out of bound Exception handled");
+        //     System.out.println(e);
+        // }
+
+        // catch(InputMismatchException e){
+        //     System.out.println("Input mismatch Exception handled");
+        //     System.out.println(e);
+        // }
+
+        finally{
+            System.out.println("Hiiii...Exception");
+        }
+        System.out.println("End of the program");
+    }
+}
