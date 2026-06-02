b258b308de1d8a918fd4b55eee9b40febbf303e4
Nidhisaa <senthilnidhi57@gmail.com>
2026-06-02 12:06:10 +0530

    Refactor SLL class: implement insertAtPosition method and update display logic

--- DataStructures/SinglyLL.java
+++ DataStructures/SinglyLL.java
@@ -2,7 +2,6 @@ package DataStructures;
 
 // Structure of the Node
 class Node{
-    @SuppressWarnings("unused")
     int data;
     Node next;
 }
@@ -32,7 +31,6 @@ class SLL{
         }
     }
 
-    
     void insertAtBegin(int val){     //insert at begining or first
         Node newNode = new Node();
         newNode.data = val;
@@ -42,16 +40,31 @@ class SLL{
         head = newNode;
     }
 
-    @SuppressWarnings("unused")
     void insertAtPosition(int val, int pos){  // insert at position
         Node newNode = new Node();
         newNode.data = val;
+        if(pos == 1){
+            newNode.next = head;
+            head = newNode;
+        }else{
+            Node temp = head;
+            for(int i=1;i<pos-1;i++){
+                temp = temp.next;
+            }
+            newNode.next = temp.next;
+            temp.next = newNode;
+        }
+    }
+
+
+    void display(){
         Node temp = head;
-        for(int i=1;i<pos-1;i++){
+
+        while(temp != null){
+            System.out.print(temp.data + " ");
             temp = temp.next;
         }
-        newNode.next = temp.next;
-        temp.next = newNode;
+        System.out.println();
     }
 
 }
@@ -59,9 +72,32 @@ class SLL{
 public class SinglyLL {
     public static void main(String[] args) {
         SLL s1 = new SLL();
-        s1.insertAtBegin(0);
+
+        // Initial values
+        s1.insertAtEnd(20);
+        s1.insertAtEnd(40);
         s1.insertAtEnd(50);
-        s1.insertAtPosition(70, 2);
+
+        System.out.print("Original List: ");
+        s1.display();
+
+        // Insert at beginning
+        s1.insertAtBegin(10);
+        System.out.print("Insert at Begining: ");
+        s1.display();
+
+        // Insert at end
+        s1.insertAtEnd(60);
+        System.out.print("Insert at End: ");
+        s1.display();
+
+        // Insert 30 at position 3
+        s1.insertAtPosition(30, 3);
+        System.out.print("Insert at Position: ");
+        s1.display();
+
+        System.out.print("After Insertions: ");
+        s1.display();
     }
     
 }
