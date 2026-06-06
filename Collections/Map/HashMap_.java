64e06384132bb3e05814f8b06ebd6c5e6de8fa04
Nidhisaa <senthilnidhi57@gmail.com>
2026-06-06 12:16:35 +0530

    Add HashMap_ class demonstrating basic HashMap operations

---
+++ Collections/Map/HashMap_.java
@@ -0,0 +1,20 @@
+package Collections.Map;
+import java.util.HashMap;
+public class HashMap_ {
+    public static void main(String[] args) {
+        HashMap<String, Integer> map = new HashMap<>();
+        map.put("a", 1);
+        map.put("b", 2);
+        map.put("c", 3);
+        map.put("d", 4);
+        map.put("e", 5);
+        map.put("f", 6);
+        map.put("g", 7);
+        map.put("g", 8);
+        map.put("i", 9);
+        map.put("j", 10);
+        System.out.println(map);
+    }
+
+    
+}
