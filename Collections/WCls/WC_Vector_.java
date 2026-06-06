5b3978ceca208f3fb47c0c537885f39ab72d1628
Nidhisaa <senthilnidhi57@gmail.com>
2026-06-06 11:29:49 +0530

    Remove WC_Vector_ and WrapperClsImplementation_ classes and their implementations

--- Collections/WCls/WC_Vector_.java
+++ Collections/WCls/WC_Vector_.java
@@ -1,4 +1,4 @@
-package Collections;
+package Collections.WCls;
 import java.util.ArrayList;
 
 public class WC_Vector_ {
--- Collections/WCls/WrapperClsImplementation_.java
+++ Collections/WCls/WrapperClsImplementation_.java
@@ -1,4 +1,4 @@
-package Collections;
+package Collections.WCls;
 
 import java.util.*;
 // import java.util.List;
