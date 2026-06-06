e533d3811ac65c80253902bce7df7345c138e87a
Nidhisaa <senthilnidhi57@gmail.com>
2026-06-06 10:21:59 +0530

    Add PriorityQueue_ class demonstrating basic priority queue operations

---
+++ Collections/Queue/PriorityQueue_.java
@@ -0,0 +1,27 @@
+package Collections.Queue;
+
+import java.util.PriorityQueue;
+public class PriorityQueue_ {
+   
+
+public class PriorityQue {
+    public static void main(String[] args) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        pq.add(10);
+        pq.add(5);
+        pq.add(15);
+        pq.add(3);
+        System.out.println(pq);
+        System.out.println(pq.poll());
+        System.out.println(pq.poll());
+
+        System.out.println(pq);
+
+        int num = pq.element();
+        System.out.println(num);
+
+        System.out.println(pq.toString());
+    }
+}
+    
+}
