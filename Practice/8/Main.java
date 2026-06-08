f534b9f37bc25ef9d40910ce84bf83d80c5693d7
Nidhisaa <senthilnidhi57@gmail.com>
2026-06-08 19:05:07 +0530

    Add initial Main class with LinkedTreeSet integration

---
+++ Practice/8/Main.java
@@ -0,0 +1,10 @@
+/*package Practice;
+import Collections.Set.LinkedTreeSet.*;
+public class Main {
+    public static void main(String[] args) {
+        LinkedandTreeSet.main(args);
+
+        
+        
+    }
+}*/
